4f2d01b809b06f9f463defa23815f760e8a4e642
Andreas Braumann <andreas@kaleido.ai>
2021-12-20 16:30:58 +0100

    - test OSTYPE

--- test/test.sh
+++ test/test.sh
@@ -3,6 +3,8 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
+echo $OSTYPE
+
 FILE=./test/files/test-removebg.png
 rm -rf $FILE
 dist/remove-bg-cli-win.exe test/files/test.jpg --api-key=$REMOVEBG_API_KEY 
